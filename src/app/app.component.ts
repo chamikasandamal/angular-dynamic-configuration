9d9e5c7ff8bc296e03350f7be9653f696dd0cf2a
Chamika Sandamal Somasiri <chamikasandamal@gmail.com>
2021-01-07 23:35:40 +0530

    use angular configuration service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConfigurationService } from './config/configuration.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  sampleConfigValue: string | undefined;
   title = 'angular-dynamic-configuration';
+  constructor(configService: ConfigurationService) {
+    this.sampleConfigValue = configService.config?.sampleConfigValue;
+  }
 }
